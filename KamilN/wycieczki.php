e00d114f903c07847dc47a38c55d19e723527e12
kamilniedek <kamil.niedek@zsbem.edu.pl>
2025-11-03 16:04:46 +0100

    Add files via upload

---
+++ KamilN/wycieczki.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki po Europie</title>
+    <link rel="stylesheet" href="styl4.css">
+</head>
+<body>
+    <header><h1>BIURO TURYSTYCZNE</h1></header>
+    <section>
+        <h3>Wycieczki, na które są wolne miejsca</h3>
+        <ul>
+            <li>
+            <li>
+    </section>
+    <aside id="lewy">
+        <h2>Bestselery</h2>
+        <table>
+
+        </table>
+    </aside>
+    <main>
+        <h2>Nasze zdjęcia</h2>
+        <div id="img"></div>
+    </main>
+    <aside id="prawy">
+        <h2>Skontaktuj się</h2>
+        <a href="turysta@wycieczki.pl">napisz do nas</a>
+        <p>telefon: 111222333</p>
+    </aside>
+    <footer>
+        <p>Stronę wykonał: Kamil Niedek</p>
+    </footer>
+    <?php
+    $conn = MySQLi_connect("localhost","root","","biuro")
+    ?>  
+</body>
+</html>
